dd917f9ed7b04eadc79321f7e77a9458b9a82e71
cocoparoma <bnbz201@naver.com>
2024-07-15 17:55:43 +0900

    서버 수정 2024-07-15
    쓰레드 나중에 사용해도 될거같고
    명령필요없으면 안써도 될거같아용

--- Post_It/Server/ServerSocket_Server.java
+++ Post_It/Server/ServerSocket_Server.java
@@ -1,92 +1,106 @@
 package post_It.Server;
 
+
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.net.InetSocketAddress;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ServerSocket_Server{
 
-    final int PORT = 9090;
-    List<Socket> list = new ArrayList<>();
-    ServerSocket serverSocket = null;
-    Socket socket = null;
+    //ServerSocket(int port) : 지정된 포트 번호에 바인딩(고정된) 소켓 만듬 (연결 대기열 최대 50)
+    //ServerSocket(int port, int backlog): 지정된 포트 번호에 바인드 된 서버 소켓을 만들고 대기중인 최대 연결 수를 backlog 매개변수로 지정
+    //참고예제
+    //https://hunit.tistory.com/256?source=post_page-----d5b5a27a50a0--------------------------------
 
-    void init(){
+    public void serverRun(){
+
+        int PORT = 9000;
+        ServerSocket server = null;
+        Socket socket = null;
+    
+        ObjectInputStream ois = null;
+        InputStream is = null;
+        //socket.getInputStream();
+        //new ObjectInputStream(is);
+        
+
+
         try {
-            //서버용 소켓 객체 하나 생성 
-            serverSocket = new ServerSocket(PORT);
-            //정보를 받아줄 스트림 2개 많들어 줬음
-
-            InputStream is = socket.getInputStream();
-            ObjectInputStream ois = new ObjectInputStream(is);
+            server = new ServerSocket(PORT);
             
-            //
-            // 예시
-            while (true) { 
-                //접속 무한 대기
-                System.out.println("[waiting...]");
-                //접속시 진행
-                socket = serverSocket.accept();
-                //ip 화면에 뿌려주기
-                InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
-                System.out.println("[connected : " + isa.getHostName() + "]");
+            while (true) {
 
-                //상대방에게서 명령할 숫자(byte or int), ID, PW, 올듯
-                //byte가 무엇인지 확인하고 switch 만들어가지고 거기다 넣어주면 될...듯?
-                int receive = ois.read();
-                
-                getOrder(receive); // 여기다 숫자 넣어서 원하는 명령을 해주면 됨 ex 0 회원가입 1로그인 2게시글 작성...
-                
-                
-            }
+                //접속대기 & 접속 후 연결 확인 메시지
+                System.out.println("[Server Waiting]");
+                socket =  server.accept();
+                System.out.println("["+socket.getInetAddress() + " : User Connected]");
 
-  /*        Thread loginThread = new Thread(()->{
-                try {
-                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                    //접속시 아이디, 비밀번호를  받아줘야 함
-                    //아이디를db쪽에서 파일리더 가지고 와서 확인해주고
-                    //옳으면 ok사인으로 int 1 리턴 해주면 됨
-                    //그리고 로그인토큰 주면됨
-                    String login_token = ois.readUTF();
-                    //Login lg = new Login();
-                    //lg.ERR_reason(4);
-                    //if(lg == 0) >> 로크인 오케이 토큰 가져가용
-                    //if( lg != 0) >> a 반환 login_CLinet에 원인 있음
-                    System.out.println("로그인토큰 예시");
-                    System.out.println("[ID, 날짜, 권한(어드민 2, 일반 1, 게스트0반환)]");
-                    //로그인 틀리면 syso(a); << 로그인 class보면 a값 반환 하는 곳있음 ERR_reason
-                    //접속 성공하면 Thread 용 list 만들어 넣어주면 되나봐용
-                } catch (Exception e) {
-                    
+                //클라이언트가 준 값 받을 준비
+                is = socket.getInputStream();
+                ois = new ObjectInputStream(is);
+
+                //값을 받아줌 
+                String client_order = ois.readUTF(); // 명령이 담겨져 있음 0 = 로그인 1 회원가입 2... 뭐 등등
+                String data =  ois.readUTF(); // 값이 담겨져 있음
+
+                int do_order = Integer.parseInt(client_order);
+                //값을 분류해줌
+                switch (do_order) {
+                    case 1:
+                        login(socket ,data);
+                        break;
+                
+                    default:
+                        break;
                 }
 
-
-
-
-
-
-
-
-            });
-            loginThread.start();
-        */       
-
+            }
         } catch (Exception e) {
-            // TODO: handle exception
+            
+        }finally{
+            try {
+                ois.close();
+                is.close();
+                server.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        
-        
-    }//init done
 
-    void getOrder(int order){
 
     }
 
+    private void login(Socket socket, String data) {
 
+        //파일 보낼 준비
+		OutputStream os = null;
+		ObjectOutputStream oos = null;
+
+        // socket.getOutputStream();
+        // new ObjectOutputStream(os);
+
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+            oos.close();
+            os.close();
+            socket.close();
+           } catch (Exception e) {
+            e.printStackTrace();
+           }
+        }
+    }
+
+
+    
 
 
 }
